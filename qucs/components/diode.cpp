948e0e67513458ae69326bd0e24c1aae3d9a0e4b
Vadim Kuznetsov <ra3xdh@gmail.com>
2026-08-18 08:54:33 +0300

    Fix diode default BV

--- qucs/components/diode.cpp
+++ qucs/components/diode.cpp
@@ -54,7 +54,7 @@ Diode::Diode()
 	QObject::tr("flicker noise exponent")));
   Props.append(new Property("Ffe", "1.0", false,
 	QObject::tr("flicker noise frequency exponent")));
-  Props.append(new Property("Bv", "10", false,
+  Props.append(new Property("Bv", "1000", false,
 	QObject::tr("reverse breakdown voltage")));
   Props.append(new Property("Ibv", "1 mA", false,
 	QObject::tr("current at reverse breakdown voltage")));
